065beecbfc0f4cc37abf16751fadcb7e9d7b05a9
Immanuel Bintang <106735816+immanuelbint@users.noreply.github.com>
2023-04-21 14:15:25 +0700

    Update yarn-tools.sh

--- yarn-tools.sh
+++ yarn-tools.sh
@@ -9,11 +9,14 @@ read -r -p "Ketikkan tool yarn yang ingin digunakan, example yarn list, yarn kil
 if [[ "$respon" =~ ^(yarnkill|yarn kill|Yarnkill|Yarn kill|YarnKill|Yarn Kill)$ ]]
 then
     read -r -p "Ketikkan applicationID => " response
-    yarn application -kill $response
+    yarn application -kill $response || echo "error applicationID not found"
 elif [[ "$respon" =~ ^(yarnlogs|yarn logs|Yarnlogs|Yarn logs|YarnLogs|Yarn Logs|yarnlog|yarn log|Yarn log|Yarn Log)$ ]]
 then
     read -r -p "Ketikkan applicationID => " response
-    yarn logs -applicationId $response
-else [[ "$respon" =~ ^(yarnlist|yarn list|Yarnlist|Yarn list|YarnList|Yarn List)$ ]]
+    yarn logs -applicationId $response || echo "error applicationID not found"
+elif [[ "$respon" =~ ^(yarnlist|yarn list|Yarnlist|Yarn list|YarnList|Yarn List)$ ]]
+then
     yarn app -list
+else
+    echo "Please enter valid command"; exit 0
 fi
